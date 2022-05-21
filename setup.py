ef6f85a01f4ac0006f81403fa87efb61a75defb2
Ed Summers <ehs@pobox.com>
2022-05-21 08:03:31 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # vim: set sw=4 et:
 from setuptools import setup, find_packages
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 def load_requirements(filename):
     with open(filename, "rt") as fh:
